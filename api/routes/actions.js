3051c9dcd93ec96777b51bf32d0933af9db95ae1
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-22 09:33:20 +0000

    MVP: Create endpoints for retrieving actions and action by id

--- api/routes/actions.js
+++ api/routes/actions.js
@@ -4,4 +4,29 @@ const actionDb = require('../../data/helpers/actionModel');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  actionDb.get()
+      .then((actions) => {
+        res.status(200).json(actions);
+      })
+      .catch((error) => {
+        res.status(500).json({ error: 'The actions could not be retrieved.' });
+      });
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  actionDb.get(id)
+      .then((action) => {
+        if (!action) {
+          res.status(404).json({ message: 'The action with the specified ID does not exist.' });
+        } else {
+          res.status(200).json(action)
+        }
+      })
+      .catch((error) => {
+        res.status(500).json({ error: 'The action information could not be retrieved.' });
+      });
+});
+
 module.exports = router;
