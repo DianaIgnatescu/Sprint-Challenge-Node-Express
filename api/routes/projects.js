f2d23e657d7c9e91d6a0e7b7fd5d8ed64d86516b
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-22 09:10:08 +0000

    MVP: Create endpoint to add and update a project

--- api/routes/projects.js
+++ api/routes/projects.js
@@ -4,7 +4,7 @@ const projectDb = require('../../data/helpers/projectModel');
 
 const router = express.Router();
 
-module.exports = router;
+
 
 router.get('/', (req, res) => {
   projectDb.get()
@@ -30,3 +30,41 @@ router.get('/:id', (req, res) => {
         res.status(500).json({ error: 'The project information could not be retrieved.' });
       });
 });
+
+router.post('/', (req, res) => {
+  const { name, description } = req.body;
+  if (!name || !description) {
+    res.status(400).json({ errorMessage: 'Please provide a name and description for the project' });
+  } else {
+    projectDb.insert({ name, description })
+        .then((data) => {
+          res.status(201).json(data);
+        })
+        .catch((error) => {
+          res.status(500).json({ error: 'There was an error while saving the project to the database.' });
+        });
+  }
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const project = req.body;
+  const { name, description } = req.body;
+  if (!name || !description) {
+    res.status(400).json({ errorMessage: 'Please provide a name and description for the project.' });
+  } else {
+    projectDb.update(id, project)
+        .then((data) => {
+          if (!data) {
+            res.status(404).json({message: 'The project with the specified ID does not exist.'});
+          } else {
+            res.status(200).json({project: {id, ...project}});
+          }
+        })
+        .catch((error) => {
+          res.status(500).json({error: 'The project information could not be modified.'});
+        });
+  }
+});
+
+module.exports = router;
