d890093235c20813864dc50806c6f75ef53d56da
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-22 09:48:15 +0000

    MVP: Create endpoints for adding and updating an action

--- api/routes/actions.js
+++ api/routes/actions.js
@@ -29,4 +29,40 @@ router.get('/:id', (req, res) => {
       });
 });
 
+router.post('/', (req, res) => {
+  const { project_id, description, notes } = req.body;
+  if (!project_id || !description || !notes) {
+    res.status(400).json({ errorMessage: 'Please provide a project ID, description, and notes for the action.' });
+  } else {
+    actionDb.insert({ project_id, description, notes })
+        .then((data) => {
+          res.status(201).json(data);
+        })
+        .catch((error) => {
+          res.status(500).json({ error: 'There was an error while saving the action to the database.' });
+        });
+  }
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const action = req.body;
+  const { project_id, description, notes } = req.body;
+  if(!project_id || !description || !notes) {
+    res.status(400).json({ errorMessage: 'Please provide a project_id, description, and notes for the action.' });
+  } else {
+    actionDb.update(id, action)
+        .then((data) => {
+          if (!data) {
+            res.status(404).json({ message: 'The action with the specified ID does not exist.' });
+          } else {
+            res.status(200).json({ action: { id, ...action }});
+          }
+        })
+        .catch((error) => {
+          res.status(500).json({ error: 'The action information could not be modified.' });
+        });
+  }
+});
+
 module.exports = router;
